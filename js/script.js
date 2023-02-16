1b8316d484fa6712d6fb4d60d5482d8305502663
vivi-barba <vitoriabbarbosa31@gmail.com>
2023-02-16 02:07:02 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+ function consultaEndereco(){
+            let cep = document.querySelector('#cep').value;
+
+            if(cep.length !==8){
+              alert("Cep inválido!");
+              return;
+            }
+
+            let url = `https://viacep.com.br/ws/${cep}/json/`;
+
+            fetch(url).then(function(response){
+                response.json().then(mostrarEndereco);
+            });
+        }
+
+        function mostrarEndereco(dados){
+            let resultado = document.querySelector('#resultado');
+            if (dados.erro){
+                resultado.innerHTML = "Não foi possível localizar o endereço!";
+            } else{
+                resultado.innerHTML = ` <p>Endereço: ${dados.logradouro}</p>
+                                        <p>Complemento: ${dados.complemento}</p>
+                                        <p>Bairro: ${dados.bairro}</p>
+                                        <p>Cidade: ${dados.localidade} - ${dados.uf}</p>`;
+                }
+               
+         }
+
+
+ 
+         
+
+
+
+
+
+
+
+
+
+         
